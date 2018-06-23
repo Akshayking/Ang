5123a587ff22bfebf74403f6662e22a648fa4215
akshay kingare <37980049+Akshayking@users.noreply.github.com>
2018-06-23 03:11:24 -0700

    Add files via upload

---
+++ assigen-2/src/app/app.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styles: ['.a{color:blue;}']
+})
+export class AppComponent {
+  title = 'Marvellous Infosystems';
+}
